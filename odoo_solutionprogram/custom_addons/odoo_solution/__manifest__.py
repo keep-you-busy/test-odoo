376d7fa835c39a7429e54f02dda43904d354a50e
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-03-23 20:04:17 +0700

    First commit

---
+++ odoo_solutionprogram/custom_addons/odoo_solution/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Test',
+    'version': '1.0.0',
+    'category': 'Tests',
+    'aithor': 'Odoo Solution',
+    'sequence': -100,
+    'summary': 'Test internal machinery',
+    'description': """
+This module is a test module based by sale_management module in odoo.
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ odoo_solutionprogram/custom_addons/odoo_solution/models/test.py
@@ -0,0 +1,26 @@
+from odoo import fields, models, api
+import random
+from odoo.exceptions import ValidationError
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    test = fields.Char(string='Test', readonly=True, states={'draft': [('readonly', False)]})
+
+    @api.onchange('order_line', 'date_order')
+    def _onchange_test(self):
+        if self.state == 'draft':
+            total = sum(line.price_total for line in self.order_line)
+            date = self.date_order.strftime('%m/%d/%Y %H:%M:%S')
+            self.test = '{} - {}'.format(total, date)
+
+    @api.model
+    def create(self, vals):
+        if 'test' not in vals:
+            vals['test'] = str(random.randint(0, 1000))
+        return super().create(vals)
+
+    def write(self, vals):
+        if 'test' in vals and len(vals['test']) > 50:
+            raise ValidationError('Длина текста должна быть меньше 50 символов!')
+        return super().write(vals)
